eaaf7c87ad9fcc0844b832e7eb4ee847727a1cf1
inlow2009 <60075778+inlow2009@users.noreply.github.com>
2020-05-12 09:54:52 -0500

    Push

---
+++ student_sign_in/src/components/StudentRow.vue
@@ -0,0 +1,34 @@
+<template>
+     <tr v-bind="'present' + student.present">         
+                      <td>{{student.name}}</td>
+                      <td>{{student.starID}}</td>
+                      <td><input type="checkbox" v-model="student.present" v-on:change="checked(student)"></td>
+                     </tr>
+    </tr>
+</template>
+
+<script>
+export default {
+    name: 'StudentRow',
+    props: {
+        student: Object
+    },
+    methods: {
+        checked(student) {
+            this.$emit('student-present', student)
+        }
+    }
+}
+</script>
+
+<style>
+.present-true {
+    color: gray;
+    font-style: italic;
+}
+
+.present-false {
+    font-weight: bold;
+}
+
+</style>
---
+++ student_sign_in/src/components/StudentTable.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+ <div class="card student-list m-2 p-2">
+            <h4 class="card-title">Students</h4>
+            <div id="student-table">
+                <table class="table">
+                    <tr>
+                        <th>Name</th>
+                        <th>StarID</th>
+                        <th>Present?</th>
+                    </tr>
+                                     <StudentRow 
+                                     v-for="student in students"
+                                      v-bind:key="student.name"
+                                      v-bind:student="student"
+                                      v-on:student-present="studentArrivedOrLeft"
+                                     ></StudentRow>
+                     </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import StudentRow from '@/components/StudentRow.vue'
+
+export default {
+    name: 'StudentTable',
+    components: { StudentRow },
+    
+    props:{
+        students: Array
+    },
+    methods: {
+        studentArrivedOrLeft(student) {
+            //todo emit message to parent
+            this.$emit('student-present', student)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
